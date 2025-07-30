06b83f5937bbe2ee6c90656c7a5b99fde7bfee31
Denis Dziganchuk <73368785+DenisDziganchuk@users.noreply.github.com>
2025-07-29 19:19:49 -0700

    Add 25w31a protocol version

--- api/src/main/java/com/velocitypowered/api/network/ProtocolVersion.java
+++ api/src/main/java/com/velocitypowered/api/network/ProtocolVersion.java
@@ -92,7 +92,9 @@ public enum ProtocolVersion implements Ordered<ProtocolVersion> {
   MINECRAFT_1_21_4(769, "1.21.4"),
   MINECRAFT_1_21_5(770, "1.21.5"),
   MINECRAFT_1_21_6(771, "1.21.6"),
-  MINECRAFT_1_21_7(772, "1.21.7", "1.21.8");
+  MINECRAFT_1_21_7(772, "1.21.7", "1.21.8"),
+
+  MINECRAFT_25W31A(1073742084, "25w31a");
 
   private static final int SNAPSHOT_BIT = 30;
 

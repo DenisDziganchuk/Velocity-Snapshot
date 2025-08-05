165f9dfa1d75a19698f3b3f322bb87f297ecc5b0
Denis Dziganchuk <73368785+DenisDziganchuk@users.noreply.github.com>
2025-08-05 10:33:27 -0700

    Latest snapshot: 25w32a

--- api/src/main/java/com/velocitypowered/api/network/ProtocolVersion.java
+++ api/src/main/java/com/velocitypowered/api/network/ProtocolVersion.java
@@ -94,7 +94,7 @@ public enum ProtocolVersion implements Ordered<ProtocolVersion> {
   MINECRAFT_1_21_6(771, "1.21.6"),
   MINECRAFT_1_21_7(772, "1.21.7", "1.21.8"),
 
-  MINECRAFT_25W31A(773, 260, "25w31a");
+  MINECRAFT_25W32A(773, 261, "25w32a");
 
   private static final int SNAPSHOT_BIT = 30;
 
